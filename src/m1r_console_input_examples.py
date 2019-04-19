7a02b3da11fba40d2d647930c57fc3d7160d0ce1
witonsz <witonskyzc@gmail.com>
2019-04-18 21:57:50 -0400

    Work

--- src/m1r_console_input_examples.py
+++ src/m1r_console_input_examples.py
@@ -6,8 +6,8 @@ This module demonstrates how to INPUT from the CONSOLE:
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Zach Witonsky.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 ###############################################################################
 # TODO: 2.  Read and run this program.  Then do the following problems,
